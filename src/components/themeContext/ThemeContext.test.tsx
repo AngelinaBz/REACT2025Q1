741ab6df39c2fd6ab56df402bf60a067df90dca0
Angelina Bezik <angelina1337bz@gmail.com>
2025-02-16 16:59:09 +0300

    feat: add tests for context api

---
+++ src/components/themeContext/ThemeContext.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ThemeToggle from './ThemeToggle';
+import { vi } from 'vitest';
+import { ThemeProvider } from './ThemeProvider';
+import { useTheme } from './UseTheme';
+
+const TestComponent: React.FC = () => {
+  const { theme } = useTheme();
+  return <span>{theme}</span>;
+};
+
+describe('ThemeContext', () => {
+  it('should have an initial light theme', () => {
+    render(
+      <ThemeProvider>
+        <ThemeToggle />
+      </ThemeProvider>
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent('Dark theme');
+  });
+
+  it('should toggle theme between light and dark', () => {
+    render(
+      <ThemeProvider>
+        <ThemeToggle />
+      </ThemeProvider>
+    );
+
+    const button = screen.getByRole('button');
+
+    expect(button).toHaveTextContent('Dark theme');
+
+    fireEvent.click(button);
+
+    expect(button).toHaveTextContent('Light theme');
+  });
+
+  it('should use the context', () => {
+    render(
+      <ThemeProvider>
+        <TestComponent />
+      </ThemeProvider>
+    );
+
+    expect(screen.getByText('light')).toBeInTheDocument();
+  });
+
+  it('should throw an error when used outside of ThemeProvider', () => {
+    const errorSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
+    const renderTestComponent = () => render(<TestComponent />);
+
+    expect(renderTestComponent).toThrow(
+      'useTheme must be used within a ThemeProvider'
+    );
+
+    errorSpy.mockRestore();
+  });
+});
