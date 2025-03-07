a1192fab29c80049e082cf7ce3c436bb737303c1
Angelina Bezik <angelina1337bz@gmail.com>
2025-03-07 17:38:30 +0300

    fix: fix search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -5,9 +5,9 @@ import React, { useEffect, useState } from 'react';
 
 import { useSearchQuery } from '@/hooks/useSearchQuery';
 
+import ErrorMessage from '../errorBoundary/ErrorMessage';
 import ThemeSelector from '../themeContext/ThemeSelector';
 import { useTheme } from '../themeContext/UseTheme';
-
 import './Search.module.css';
 
 const Search = () => {
@@ -17,6 +17,7 @@ const Search = () => {
   const queryParam = searchParams?.get('query');
   const [query, setQuery] = useSearchQuery();
   const [search, setSearch] = useState<string>(queryParam || query);
+  const [hasError, setHasError] = useState<boolean>(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
@@ -26,6 +27,7 @@ const Search = () => {
     if (query && !queryParam) {
       const params = new URLSearchParams(searchParams?.toString());
       params.set('query', query);
+      params.set('page', '1');
       router.push(`/search/pages?${params.toString()}`);
     }
   }, [query, router, searchParams]);
@@ -35,9 +37,7 @@ const Search = () => {
     if (search) {
       params.set('query', search);
     } else params.delete('query');
-    if (search !== queryParam) {
-      params.set('page', '1');
-    }
+    params.set('page', '1');
     setQuery(search);
     router.push(`/search/pages?${params.toString()}`);
   };
@@ -47,8 +47,18 @@ const Search = () => {
       throw new Error('Testing Error');
     } catch (error) {
       console.error('Error caught in ErrorBoundary: ', error);
+      setHasError(true);
     }
   };
+
+  const handleCloseError = () => {
+    setHasError(false);
+    router.push('/search');
+  };
+
+  if (hasError) {
+    return <ErrorMessage onClose={handleCloseError} />;
+  }
 
   return (
     <section className="search-container">
